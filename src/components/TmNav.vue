cb3f7838389836b920e8b795955bcf4f07709f98
Shruan <qiushiyuan1994@qq.com>
2017-09-25 22:07:22 +0800

    新建左侧导航组件

---
+++ src/components/TmNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="nav">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
