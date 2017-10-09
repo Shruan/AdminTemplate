ce781d8c206ec4596c53a57d090d4a2f2176256e
Shruan <qiushiyuan1994@qq.com>
2017-10-09 10:02:57 +0800

    路由新增

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router'
 import Login from '@/pages/Login'
 import Index from '@/pages/Index'
 import Home from '@/pages/Home/Index'
+import ManageList from '@/pages/Home/ManageList'
+import SupplierList from '@/pages/Home/SupplierList'
+import LoginLog from '@/pages/Home/LoginLog'
+import ImportPatent from '@/pages/Home/ImportPatent'
+import SellPatentLog from '@/pages/Home/SellPatentLog'
 
 Vue.use(Router)
 
@@ -18,7 +23,12 @@ export default new Router({
       name: 'Index',
       component: Index,
       children: [
-        { path: '/home/index', component: Home }
+        { path: '/home/index', component: Home },
+        { path: '/home/manage', component: ManageList },
+        { path: '/home/supplier', component: SupplierList },
+        { path: '/home/loginLog', component: LoginLog },
+        { path: '/home/importPatent', component: ImportPatent },
+        { path: '/home/sellPatentLog', component: SellPatentLog }
       ]
     }
   ]
