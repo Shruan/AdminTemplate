bdaeb3656b92207d2b98fd46d0431f1bfafe42d7
Shruan <qiushiyuan1994@qq.com>
2018-04-16 02:35:46 +0800

    引入最新版本Element Ui css

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import store from './store/index.js'
 import axios from 'axios'
-import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 Vue.directive('myfocus', {
