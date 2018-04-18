e27a78c100cb6d6912b050d1590645feb19cf921
Shruan <qiushiyuan1994@qq.com>
2018-04-19 02:38:53 +0800

    增加全局变量

---
+++ src/assets/globalData.js
@@ -0,0 +1,12 @@
+// 全局数据
+
+module.exports = {
+  // 专利类型
+  patentTypeList: [
+    { name: '发明专利', value: '1' },
+    { name: '实用新型', value: '2' },
+    { name: '外观专利', value: '3' },
+    { name: 'PCT发明', value: '4' },
+    { name: 'PCT实用', value: '5' }
+  ]
+}
