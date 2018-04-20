bb918a39d38afb0b342e4da7d77b765da7c4e982
Shruan <qiushiyuan1994@qq.com>
2018-04-21 00:15:50 +0800

    增加全局变量

--- src/assets/globalData.js
+++ src/assets/globalData.js
@@ -8,5 +8,15 @@ module.exports = {
     { name: '外观专利', value: '3' },
     { name: 'PCT发明', value: '4' },
     { name: 'PCT实用', value: '5' }
+  ],
+
+  patentStatusList: [
+    { name: '授权未下证', value: '授权未下证' },
+    { name: '专利权维持', value: '专利权维持' },
+    { name: '等年费滞纳金', value: '等年费滞纳金' },
+    { name: '视为放弃，等恢复', value: '视为放弃，等恢复' },
+    { name: '审核中', value: '审核中' },
+    { name: '无效', value: '无效' },
+    { name: '其他', value: '其他' }
   ]
 }
