d24eff4220fd839bad4537ff5850d920dc31da16
Shruan <qiushiyuan1994@qq.com>
2018-06-27 12:56:28 +0800

    组件内请求模块抽离

--- src/api/model/myActivity.js
+++ src/api/model/myActivity.js
@@ -2,7 +2,7 @@ import request from '../request'
 
 export function getActivityList (data) {
   return request.get(
-    '/Api/patent/adminlist/',
-    data
+    '/Api/patent/adminlist',
+    { params: data }
   )
 }
